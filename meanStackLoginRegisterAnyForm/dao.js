14ac3dba4db7fd5b70f700d481a6c7dff0fd6e03
ashamanimaran <asha_manimaran@yahoo.com>
2016-04-13 13:45:35 -0700

    complete login register functionality

---
+++ meanStackLoginRegisterAnyForm/dao.js
@@ -0,0 +1,39 @@
+/// <reference path="typings/browser/ambient/mongoose/index.d.ts" />
+/// <reference path="typings/browser/ambient/node/index.d.ts" />
+
+var mongoose = require('mongoose');
+    var db = mongoose.connect('mongodb://localhost/bookshop');
+    var Schema = mongoose.Schema;
+//the schema represents the table schema(DDL) in mongoose
+    var UserInfo = new Schema({
+    "user" : String,
+    "pwd" : String,
+    "firstName":String,
+    "lastName":String
+    });
+ //the model represents the table/collection in mongoose
+ //you use the model to interact with the actual table/collection
+ //mongoose pluralises the table/collection name. If the name of ur table is logins, you should specify only 'login' as table name
+ 
+    var LoginMapper=mongoose.model('login', UserInfo);
+//find record
+/*
+ LoginInfo.find({},function (err,res) {
+        console.log(res);
+    })
+ */
+//add record
+/*
+LoginInfo.create({"user":"tim","pwd":"tim1234"},function(err,res){
+     console.log(res);
+})
+*/
+//delete record
+/*
+LoginInfo.remove({"user":"tim"},function(err,res){
+    console.log(res);
+})*/
+
+module.exports=LoginMapper;
+
+
---
+++ meanStackLoginRegisterAnyForm/server.js
@@ -0,0 +1,73 @@
+/// <reference path="typings/browser/ambient/express-serve-static-core/index.d.ts" />
+/// <reference path="typings/browser/ambient/serve-static/index.d.ts" />
+/// <reference path="typings/browser/ambient/express/index.d.ts" />
+/// <reference path="typings/browser/ambient/node/index.d.ts" />
+/// <reference path="typings/browser/ambient/body-parser/index.d.ts" />
+var express=require('express');
+var server=express();
+var bodyparser=require('body-parser');
+server.use(bodyparser.urlencoded({extended: true})); 
+var LoginMapper=require('./dao');
+server.get("/",function(req,res){
+    res.sendFile(__dirname+"/login.html");
+});
+server.get("/app.js",function(req,res){
+    res.sendFile(__dirname+"/app.js");
+});
+server.get("/register.html",function(req,res){
+    res.sendFile(__dirname+"/register.html");
+});
+
+server.post("/login",function(req,res){
+   var user= req.body.user;
+   var pwd=req.body.pwd;
+   console.log(user+","+pwd);
+   LoginMapper.find({"user":user,"pwd":pwd},function(err,result){
+       if(err!=null){
+        res.status(500);
+         res.send('<h1>Mongoose server not running</h1>');
+      }else{
+           if(result.length==0){
+                       res.status(400);
+                      res.send('<h1>user not registered</h1>');  
+           }else{
+                    res.status(200);
+                   res.send(result);         
+           }
+
+       }
+    
+   });
+});
+server.post("/register",function(req,res){
+   var user= req.body.user;
+   var pwd=req.body.pwd;
+   var firstName= req.body.firstName;
+   var lastName=req.body.lastName;
+   console.log(user+","+pwd);
+   LoginMapper.find({"user":user},function(err,result){
+       if(err!=null){
+         res.status(500);
+         res.send('<h1>Mongoose server not running</h1>');
+       }else{
+           if(result.length==0){
+               //create a new user with all details taken from modal form
+                      LoginMapper.create({"user":user,"pwd":pwd,"firstName":firstName,"lastName":lastName},function(err,res){
+                       console.log("new user created!")   
+                      });
+                       res.status(200);
+                      res.sendFile(__dirname+"/welcome.html");   
+           }else{
+                    res.status(400);
+                    res.send('<h1>choose a different login</h1>');         
+           }
+
+       }
+    
+   });
+});
+server.listen("9999",'localhost');
+
+
+
+
